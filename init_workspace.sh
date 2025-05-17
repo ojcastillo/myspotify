a25199528c84c300d9a85c05bdc34c87ccf02333
Orlando Castillo <jcm.orlando@gmail.com>
2025-05-17 13:56:54 -0700

    fix: Make init_workspace.sh use the active python on terminal

--- init_workspace.sh
+++ init_workspace.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Setting up virtual environment..."
+python -m venv .venv
 
-virtualenv -p python .venv
+echo "Activating virtual environment..."
 source .venv/bin/activate
+
+echo "Installing dependencies..."
 pip install -r requirements-dev.txt
 
-echo "Your virtual environment should now be active!"
-echo "To deactivate run: `deactivate`"
+echo "Your virtual environment should now be active and ready to use!"
+echo "To deactivate run: 'deactivate'"
